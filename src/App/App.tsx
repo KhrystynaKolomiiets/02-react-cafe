8cee7e40ddf69266da395cfeee158a3b935963e8
KhrystynaKolomiiets <kaulitz89996@gmail.com>
2025-07-24 16:52:49 +0200

    add homework

---
+++ src/App/App.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { type VoteType } from "../types/votes";
+import CafeInfo from "../CafeInfo/CafeInfo";
+import css from "./App.module.css";
+import VoteOptions from "../VoteOptions/VoteOptions";
+import { type Vote } from "../types/votes";
+import VoteStatus from "../VoteStatus/VoteStatus";
+import Notification from "../Notification/Notification";
+export default function App() {
+  const defaultVotes: Vote = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  const [votes, setVotes] = useState<Vote>(defaultVotes);
+  const handleVote = (type: VoteType) => {
+    setVotes({ ...votes, [type]: votes[type] + 1 });
+  };
+  const resetVotes = () => {
+    setVotes(defaultVotes);
+  };
+  const totalVotes = votes.good + votes.neutral + votes.bad;
+  const positiveRate = totalVotes
+    ? Math.round((votes.good / totalVotes) * 100)
+    : 0;
+
+  return (
+    <div className={css.app}>
+      <CafeInfo />
+      {totalVotes > 0 ? (
+        <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={true} />
+      ) : (
+        <VoteOptions
+          onVote={handleVote}
+          onReset={resetVotes}
+          canReset={false}
+        />
+      )}
+      {totalVotes > 0 ? (
+        <VoteStatus
+          votes={votes}
+          totalVotes={totalVotes}
+          positiveRate={positiveRate}
+        />
+      ) : (
+        <Notification />
+      )}
+    </div>
+  );
+}
---
+++ src/VoteStatus/VoteStatus.tsx
@@ -0,0 +1,33 @@
+import type { Vote } from "../types/votes";
+import styles from "./VoteStatus.module.css";
+
+interface VoteStatsProps {
+  votes: Vote;
+  totalVotes: number;
+  positiveRate: number;
+}
+export default function VoteStatus({
+  votes: { good, neutral, bad },
+  totalVotes,
+  positiveRate,
+}: VoteStatsProps) {
+  return (
+    <div className={styles.container}>
+      <p className={styles.stat}>
+        Good: <strong>{good}</strong>
+      </p>
+      <p className={styles.stat}>
+        Neutral: <strong>{neutral}</strong>
+      </p>
+      <p className={styles.stat}>
+        Bad: <strong>{bad}</strong>
+      </p>
+      <p className={styles.stat}>
+        Total: <strong>{totalVotes}</strong>
+      </p>
+      <p className={styles.stat}>
+        Positive: <strong>{positiveRate}%</strong>
+      </p>
+    </div>
+  );
+}
